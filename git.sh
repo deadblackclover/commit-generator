0bfc60f1ad3bfd57d2d2b373234b2223f3fb69b2
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-04-03 10:31:45 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -3,14 +3,14 @@
 for (( i=$2; i >= 1; i-- ))
 do
 
-if [ $(($2 % 2)) -eq 0 ]
-then
-echo " " >> $1
-else
-head -n -1 $1
-fi
+if [ $(($i % 2)) -eq 0 ]
+    then
+    echo "$1" >> $1
+    else
+    sed -i "$ d" test
+    fi
 
-git add .
-date_commit=$(date +"%s" -d "$i day ago")
-GIT_COMMITTER_DATE="$date_commit" git commit -m "Change $1" --date "$date_commit"
+    git add .
+    date_commit=$(date +"%s" -d "$i day ago")
+    GIT_COMMITTER_DATE="$date_commit" git commit -m "Change $1" --date "$date_commit"
 done
